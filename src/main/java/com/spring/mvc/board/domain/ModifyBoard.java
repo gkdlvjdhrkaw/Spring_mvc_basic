1f15b455d17249155b3bee1ae1c1cf3de91f6591
Choco SSong-i <gkdlvjdhrkaw@gmail.com>
2022-01-17 22:49:45 +0900

    [22.01.17] # Spring Web board APP
    [22.01.17] # Spring Web board APP

---
+++ src/main/java/com/spring/mvc/board/domain/ModifyBoard.java
@@ -0,0 +1,13 @@
+package com.spring.mvc.board.domain;
+
+public class ModifyBoard {
+
+    public int getBoardNo() {
+    }
+
+    public String getTitle() {
+    }
+
+    public String getContent() {
+    }
+}
